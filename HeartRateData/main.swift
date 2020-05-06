e0d9e21ab848b0191c6ed4f524c34b86ab8bc1f5
Keith Lee <lee.mingkeith@gmail.com>
2020-05-06 11:55:41 +0900

    Initial Commit

---
+++ HeartRateData/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HeartRateData
+//
+//  Created by Keith Lee on 2020/05/06.
+//  Copyright © 2020 Keith Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
