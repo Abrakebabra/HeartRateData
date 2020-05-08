773b782684c69c193fa2e781ff21e3d323884334
Keith Lee <lee.mingkeith@gmail.com>
2020-05-09 00:21:59 +0900

    First draft of file save function

--- HeartRateData/dataSaver.swift
+++ HeartRateData/dataSaver.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+
+func saveToFile(data: Data) {
+    
+    // get documents directory
+    guard let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+        
+        print("directory not found")
+        return
+    }
+    
+    
+    var fileIteration: Int = 1
+    var availableFileName: Bool = false
+    // whole url to the .json document
+    var fileURL: URL?
+    
+    
+    while availableFileName == false {
+        let stringedIteration = String(format: "%02d", fileIteration)
+        let file = "HeartRateData \(stringedIteration).json"
+        let checkingURL = directory.appendingPathComponent(file)
+        
+        //reading to see if file exists
+        do {
+            let _ = try String(contentsOf: checkingURL, encoding: .utf8)
+            fileIteration += 1
+            continue
+        }
+        catch {
+            fileURL = checkingURL
+            availableFileName = true
+            break
+        }
+        
+    } // while availableFileName
+    
+    
+    if let fileURL = fileURL {
+        do {
+            try data.write(to: fileURL, options: .atomic)
+        }
+        catch let error {
+            print(error)
+        }
+        
+    }
+    
+}
--- HeartRateData/main.swift
+++ HeartRateData/main.swift
@@ -12,21 +12,18 @@ let bleController = BLEController()
 bleController.start()
 
 var runProgram: Bool = true
-
-
+let dataSemaphore = DispatchSemaphore(value: 1)
 var bpmRecords: [Int] = []
 
 bleController.bpmReceived = {
     bpm in
     print(bpm)
     if bpm > 40 {
+        dataSemaphore.wait()
         bpmRecords.append(bpm)
+        dataSemaphore.signal()
     }
 }
-
-
-
-
 
 
 
@@ -39,10 +36,21 @@ while runProgram {
     
     switch input {
     case "end":
-        let data = serialiser(array: bpmRecords)
+        dataSemaphore.wait()
         
+        do {
+            let data = try serialiser(array: bpmRecords)
+            saveToFile(data: data)
+        }
+        catch let error {
+            print(error)
+        }
+        
+        dataSemaphore.signal()
+        
+    default:
+        continue
     }
-    
 }
 
 
